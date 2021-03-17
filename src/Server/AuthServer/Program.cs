548c5b7176de4223325f3fd2ba7bb1edefd49f2f
ryancheung <ryancheung.go@gmail.com>
2021-03-17 09:39:56 +0800

    Added AuthServer

---
+++ src/Server/AuthServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
