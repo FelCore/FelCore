57c6a6d3e8d6cc4c0619bc5f805993dedd29876c
ryancheung <ryancheung.go@gmail.com>
2021-03-28 23:09:24 +0800

    DB/Auth: Fixed datetype for field 'time' of table 'logs'

--- sql/base/fel_auth_database.sql
+++ sql/base/fel_auth_database.sql
@@ -229,7 +229,7 @@ DROP TABLE IF EXISTS `logs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `logs` (
-  `time` int unsigned NOT NULL,
+  `time` datetime NOT NULL,
   `realm` int unsigned NOT NULL,
   `type` varchar(250) NOT NULL,
   `level` tinyint unsigned NOT NULL DEFAULT '0',
