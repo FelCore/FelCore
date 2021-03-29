a90d40f1708882d6d8146385d5b8772a64e4fca6
ryancheung <ryancheung.go@gmail.com>
2021-03-29 12:35:21 +0800

    Server/AuthServer: Added IP ban checker

--- src/Server/AuthServer/AuthSession.cs
+++ src/Server/AuthServer/AuthSession.cs
@@ -9,10 +9,28 @@ using static Common.Log;
 using static Common.ConfigMgr;
 using static Common.Errors;
 using Server.Database;
+using static Server.Database.LoginStatements;
 using Server.Shared;
 
 namespace Server.AuthServer
 {
+    using static eAuthCmd;
+    using static Server.AuthServer.AuthResult;
+
+    enum eAuthCmd : byte
+    {
+        AUTH_LOGON_CHALLENGE = 0x00,
+        AUTH_LOGON_PROOF = 0x01,
+        AUTH_RECONNECT_CHALLENGE = 0x02,
+        AUTH_RECONNECT_PROOF = 0x03,
+        REALM_LIST = 0x10,
+        XFER_INITIATE = 0x30,
+        XFER_DATA = 0x31,
+        XFER_ACCEPT = 0x32,
+        XFER_RESUME = 0x33,
+        XFER_CANCEL = 0x34
+    }
+
     public class AuthSession : SocketBase
     {
         AsyncCallbackProcessor<QueryCallback> _queryProcessor;
@@ -22,10 +40,66 @@ namespace Server.AuthServer
             _queryProcessor = new AsyncCallbackProcessor<QueryCallback>();
         }
 
+        public void SendPacket(ByteBuffer packet)
+        {
+            if (!IsOpen())
+                return;
+
+            if (!packet.Empty && packet.Wpos() > 0)
+            {
+                MessageBuffer buffer = new(packet.Wpos());
+                buffer.Write(packet.Data(), packet.Wpos());
+                QueuePacket(buffer);
+            }
+        }
+
         public override void Start()
         {
             var ipAddress = RemoteAddress.ToString();
             FEL_LOG_TRACE("session", "Accepted connection from {0}", ipAddress);
+
+            var stmt = DB.LoginDatabase.GetPreparedStatement(LOGIN_SEL_IP_INFO);
+            stmt.Parameters[0] = ipAddress;
+
+            _queryProcessor.AddCallback(DB.LoginDatabase.AsyncQuery(stmt).WithPreparedCallback(CheckIpCallback));
+        }
+
+        void CheckIpCallback(PreparedQueryResult? result)
+        {
+            if (result != null)
+            {
+                bool banned = false;
+                do
+                {
+                    var fields = result.Fetch();
+                    if (fields.GetUInt64(0) != 0)
+                        banned = true;
+
+                } while (result.NextRow());
+
+                if (banned)
+                {
+                    ByteBuffer pkt = new ByteBuffer();
+                    pkt.Append((byte)AUTH_LOGON_CHALLENGE);
+                    pkt.Append((byte)0);
+                    pkt.Append((byte)WOW_FAIL_BANNED);
+                    SendPacket(pkt);
+                    FEL_LOG_DEBUG("session", "[AuthSession::CheckIpCallback] Banned ip '{0}:{1}' tries to login!", RemoteAddress.ToString(), RemotePort);
+                    return;
+                }
+            }
+
+            AsyncRead();
+        }
+
+        protected override void ReadHandler()
+        {
+            var packet = GetReadBuffer();
+            while (packet.GetActiveSize() != 0)
+            {
+                var cmd = packet.GetByte(0);
+                //TODO:
+            }
         }
 
         public override bool Update()
--- src/Server/Database/Implementation/LoginDatabaseConnection.cs
+++ src/Server/Database/Implementation/LoginDatabaseConnection.cs
@@ -15,6 +15,7 @@ namespace Server.Database
         LOGIN_SEL_REALMLIST,
         LOGIN_DEL_EXPIRED_IP_BANS,
         LOGIN_UPD_EXPIRED_ACCOUNT_BANS,
+        LOGIN_SEL_IP_INFO,
         LOGIN_INS_LOG,
         MAX_LOGINDATABASE_STATEMENTS
     }
@@ -38,6 +39,7 @@ namespace Server.Database
             PrepareStatement(LOGIN_SEL_REALMLIST, "SELECT id, name, address, localAddress, localSubnetMask, port, icon, flag, timezone, allowedSecurityLevel, population, gamebuild FROM realmlist WHERE flag <> 3 ORDER BY name", CONNECTION_SYNCH);
             PrepareStatement(LOGIN_DEL_EXPIRED_IP_BANS, "DELETE FROM ip_banned WHERE unbandate<>bandate AND unbandate<=UNIX_TIMESTAMP()", CONNECTION_ASYNC);
             PrepareStatement(LOGIN_UPD_EXPIRED_ACCOUNT_BANS, "UPDATE account_banned SET active = 0 WHERE active = 1 AND unbandate<>bandate AND unbandate<=UNIX_TIMESTAMP()", CONNECTION_ASYNC);
+            PrepareStatement(LOGIN_SEL_IP_INFO, "SELECT unbandate > UNIX_TIMESTAMP() OR unbandate = bandate AS banned, NULL as country FROM ip_banned WHERE ip = ?", CONNECTION_ASYNC);
             PrepareStatement(LOGIN_INS_LOG, "INSERT INTO logs (time, realm, type, level, string) VALUES (?, ?, ?, ?, ?)", CONNECTION_ASYNC);
         }
     }
